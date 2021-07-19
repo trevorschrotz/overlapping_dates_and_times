a1da8e39dabd64ce81f8b1524f0b7ea8d71506d9
Trevor Schrotz <49207031+trevorschrotz@users.noreply.github.com>
2021-07-19 15:25:21 -0400

    Update Overlapping DateTime Function.R

--- Overlapping DateTime Function.R
+++ Overlapping DateTime Function.R
@@ -2,11 +2,11 @@ overlap_function <- function(df) {
     df %>% #data frame of interest
         arrange(group_var_1, date_time_variable_start) %>% #arrange temporally
         group_by(group_var_1) %>% #group by variables of interest, could be several
-        mutate(index = c(0, cumsum(as.numeric(lead(date_time_variable_start)) > #the index shows which time groups the datetimes were collapsed into 
+        mutate(Index = c(0, cumsum(as.numeric(lead(date_time_variable_start)) > #the index shows which time groups the datetimes are collapsed into based on the group_by
                                       cummax(as.numeric(date_time_variable_start_end)))[-n()])) %>%
-        group_by(group_var_1, index) %>%
-        summarize(Start = min(StartTime), 
-                  End = max(EndTime),
+        group_by(group_var_1, Index) %>%
+        summarize(Start = min(date_time_variable_start), 
+                  End = max(date_time_variable_start_end),
                   TotalHours = round(as.double(difftime(End, Start, units = "hours")), 2),
                   .groups = "drop")
 }
